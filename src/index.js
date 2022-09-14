5d4389a64de6ecaeaf448a167d4bb10303c3b4c6
Xi Wenchuan <xq1033419145@163.com>
2022-09-14 15:15:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use((ctx, next) => {
         var commit = requestBodyJson.commits[0].message
         var date = requestBodyJson.commits[0].timestamp.split("T")[0]
         var detailTime = requestBodyJson.commits[0].timestamp.split("T")[1].split("+")[0]
-        var time = "**"+date+"**"+" "+"**"+detailTime+"**"
+        var time = date+" "+detailTime
         var transContent = {
             msg_type: "post",
             content: {
@@ -35,7 +35,7 @@ app.use((ctx, next) => {
                                 },
                                 {
                                     tag: "text",
-                                    text: "**"+pusher+"**",
+                                    text: pusher,
                                 },
                             ],
                             [
@@ -45,7 +45,7 @@ app.use((ctx, next) => {
                                 },
                                 {
                                     tag: "text",
-                                    text: "**"+branch+"**",
+                                    text: branch
                                 },
                             ],
                             [
@@ -55,7 +55,7 @@ app.use((ctx, next) => {
                                 },
                                 {
                                     tag: "text",
-                                    text: "**"+commit+"**",
+                                    text: commit
                                 },
                             ],
                             [
