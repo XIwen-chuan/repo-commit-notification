496de6e887aef18a8af700838e32ca6b1b359e19
Xi Wenchuan <xq1033419145@163.com>
2022-09-14 13:23:05 +0800

    配置修改

--- dockerfile
+++ dockerfile
@@ -2,7 +2,5 @@ FROM node
 WORKDIR /app
 COPY . /app
 RUN npm install
-RUN npm run compile
-FROM nginx:alpine as server
 EXPOSE 3000
 CMD npm run serve
--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@ docker build -t repo-commit-notification .
 docker run \
 -d --name repo-commit-notification \
 -p 3000:3000 \
---memory="2g" \
---memory-swap="4g" \
 --network=host \
 repo-commit-notification
